2094e07bd0a41e74984a235b1bba2efc6f648fe8
Artur <artur_goncharov_97@mail.ru>
2023-03-14 21:41:49 +0300

    added router for balance view

--- app/routers/payment_system.py
+++ app/routers/payment_system.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.datebase import get_session
+from app.models.account import Account
 
 
 payment_router = APIRouter(
@@ -7,6 +13,8 @@ payment_router = APIRouter(
 )
 
 
-@payment_router.get('/balance')
-def get_balance():
-    return 'Ваш баланс будет тут'
+@payment_router.get('/balance/account/{id:int}')
+async def get_balance(id: int, session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Account).where(Account.id == id))
+    account = result.scalar()
+    return JSONResponse({"balance": account.balance})
