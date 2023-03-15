b87124f4c8e9ea977f04a39769842cdca6d927e5
Artur <artur_goncharov_97@mail.ru>
2023-03-15 13:34:10 +0300

    added validator and BalanceShema

--- app/schemas/account.py
+++ app/schemas/account.py
@@ -1,7 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
+from typing import Optional, Literal
 
 
 class AccountShema(BaseModel):
     id: int
     name: str
     balance: int
+
+    @validator('balance')
+    def summa_must_positiv_integer(cls, v):
+        if v < 0:
+            raise ValueError("Баланс не может быть отризательным")
+        return v
+    
+    class Config:
+        orm_mode = True
+
+
+class BalanceShema(BaseModel):
+    user_id: int
+    summa: int
+    method: Literal['plus', 'minus']
+
+    @validator('summa')
+    def summa_must_positiv_integer(cls, v):
+        if v < 0:
+            raise ValueError("Сумма пополнения не может быть отризательной")
+        return v
