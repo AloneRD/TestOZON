0053613051e8ee70212d141baa6fdda7bc763725
Artur <artur_goncharov_97@mail.ru>
2023-03-31 13:45:52 +0300

    formatting

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,7 +2,7 @@ import asyncio
 
 import pytest
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_scoped_session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 from sqlalchemy_utils.functions import database_exists, create_database
@@ -32,6 +32,7 @@ async def prepare_database():
     async with engine_test.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
+
 async def override_get_session():
     async with async_session_maker() as session:
         yield session
@@ -39,12 +40,14 @@ async def override_get_session():
 app = create_app()
 app.dependency_overrides[get_session] = override_get_session
 
+
 @pytest.fixture(scope='session')
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
 
 @pytest.fixture(scope="session")
 async def test_client():
